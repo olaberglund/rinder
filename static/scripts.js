3db057733ae55139701c9460b9b8ae9223637c8f
Ola Berglund <olakjberglund@gmail.com>
2024-03-15 10:54:55 +0100

    add back script to add ingredient

--- static/scripts.js
+++ static/scripts.js
@@ -1,3 +1,16 @@
+function addIngredient(ingredient) {
+  var list = document.getElementById("recipe-ingredients");
+  if (list) {
+    const ingredients = list.value.split(/\r?\n/);
+    if (!ingredients.includes(ingredient)) {
+      const newList = ingredient + "\n" + list.value;
+      list.value = newList;
+    } else {
+      alert("Ingrediensen är redan tillagd");
+    }
+  }
+}
+
 document.getElementById("query")?.addEventListener("keyup", ({ key }) => {
   if (key === "Enter") {
     document.getElementById("search-button")?.click();
