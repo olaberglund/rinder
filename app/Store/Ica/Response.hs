27cdd7dbd4f467f3238b9a29b6475fa6abeecd46
Ola Berglund <olakjberglund@gmail.com>
2024-04-04 19:37:55 +0200

    add data type for ICA Offer

--- app/Store/Ica/Response.hs
+++ app/Store/Ica/Response.hs
@@ -1,1 +1,44 @@
 module Store.Ica.Response () where
+
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Text (Text)
+import Deriving.Aeson (CustomJSON, FieldLabelModifier, Rename, StripPrefix)
+import Deriving.Aeson qualified
+import GHC.Generics (Generic)
+
+data Offer = Offer
+    { offerId :: Text -- OfferId
+    , offerStoreId :: [Int] -- StoreId
+    , offerArticleGroup :: [Int] -- ArticleGroup
+    , offerType :: Text -- OfferType
+    , offerImageUrl :: Text -- ImageUrl
+    , offerArticles :: [Article] -- Articles
+    , offerCondition :: Text -- OfferCondition
+    }
+    deriving stock (Show, Generic, Eq)
+    deriving
+        (FromJSON, ToJSON)
+        via CustomJSON
+                '[ FieldLabelModifier
+                    '[ StripPrefix "offer"
+                     , Rename "Id" "OfferId"
+                     , Rename "Type" "OfferType"
+                     , Rename "Condition" "OfferCondition"
+                     ]
+                 ]
+                Offer
+
+data Article = Article
+    { articleId :: Text -- EanId
+    , articleDescription :: Text -- ArticleDescription
+    }
+    deriving stock (Show, Generic, Eq)
+    deriving
+        (FromJSON, ToJSON)
+        via CustomJSON
+                '[ FieldLabelModifier
+                    '[ Rename "articleId" "EanId"
+                     , Rename "articleDescription" "ArticleDescription"
+                     ]
+                 ]
+                Article
