6fd48af00ad00669b21e576d881afe4ab573ba03
Ola Berglund <olakjberglund@gmail.com>
2024-05-01 14:20:46 +0200

    redirect with language on expense removal

--- app/Server/App.hs
+++ app/Server/App.hs
@@ -6,9 +6,9 @@ module Server.App (app) where
 import Data.Text.Encoding qualified as Text
 import Inter.Language (Language (..), mkHref)
 import Servant (
-    NamedRoutes,
-    Proxy (Proxy),
-    serveDirectoryWebApp,
+  NamedRoutes,
+  Proxy (Proxy),
+  serveDirectoryWebApp,
  )
 import Servant.Server
 import Servant.Server.Generic (AsServer)
@@ -24,45 +24,45 @@ import Store.Grocery (groceryName)
 
 app :: Env -> Application
 app env =
-    serveWithContext
-        (Proxy @(NamedRoutes RootApi))
-        (customFormatters :. EmptyContext)
-        (server env)
+  serveWithContext
+    (Proxy @(NamedRoutes RootApi))
+    (customFormatters :. EmptyContext)
+    (server env)
 
 server :: Env -> RootApi AsServer
 server env =
-    RootApi
-        { homePageEP = redirect (home SE)
-        , staticEP = serveDirectoryWebApp "static"
-        , pagesEP = \lang ->
-            PagesApi
-                { languageHomePageEP = redirect (home lang)
-                , shoppingEP = \grocery ->
-                    ShoppingApi
-                        { saPageEP = shoppingPageH env lang grocery
-                        , saItemListEP =
-                            productListH
-                                lang
-                                grocery
-                                (addToShoppingList lang (groceryName grocery))
-                        , saAddItemEP = addProductH env lang grocery
-                        , saToggleItemEP = toggleProductH env lang grocery
-                        , saRemoveCheckedEP = removeCheckedH env lang grocery
-                        , saRemoveAllEP = removeAllH env lang grocery
-                        , saModifyItemNoteEP = noteProductH env lang grocery
-                        , saReorderItemEP = reorderItemH env lang grocery
-                        , saSseEP = sseH env
-                        }
-                , splitEP =
-                    SplitApi
-                        { splitPageEP = splitPageH env lang
-                        , newExpenseEP = newExpenseH env lang
-                        , settleUpEP = settleUpH env lang
-                        , editExpensePageEP = editExpensePageH env lang
-                        , saveExpenseEP = saveExpenseH env lang
-                        , removeExpenseEp = removeExpenseH env
-                        }
+  RootApi
+    { homePageEP = redirect (home SE)
+    , staticEP = serveDirectoryWebApp "static"
+    , pagesEP = \lang ->
+        PagesApi
+          { languageHomePageEP = redirect (home lang)
+          , shoppingEP = \grocery ->
+              ShoppingApi
+                { saPageEP = shoppingPageH env lang grocery
+                , saItemListEP =
+                    productListH
+                      lang
+                      grocery
+                      (addToShoppingList lang (groceryName grocery))
+                , saAddItemEP = addProductH env lang grocery
+                , saToggleItemEP = toggleProductH env lang grocery
+                , saRemoveCheckedEP = removeCheckedH env lang grocery
+                , saRemoveAllEP = removeAllH env lang grocery
+                , saModifyItemNoteEP = noteProductH env lang grocery
+                , saReorderItemEP = reorderItemH env lang grocery
+                , saSseEP = sseH env
                 }
-        }
-  where
-    home lang = Text.encodeUtf8 $ mkHref lang "/split"
+          , splitEP =
+              SplitApi
+                { splitPageEP = splitPageH env lang
+                , newExpenseEP = newExpenseH env lang
+                , settleUpEP = settleUpH env lang
+                , editExpensePageEP = editExpensePageH env lang
+                , saveExpenseEP = saveExpenseH env lang
+                , removeExpenseEp = removeExpenseH env lang
+                }
+          }
+    }
+ where
+  home lang = Text.encodeUtf8 $ mkHref lang "/split"
--- app/Server/Split/Handler.hs
+++ app/Server/Split/Handler.hs
@@ -1,164 +1,165 @@
 module Server.Split.Handler (
-    settleUpH,
-    newExpenseH,
-    splitPageH,
-    removeExpenseH,
-    saveExpenseH,
-    editExpensePageH,
+  settleUpH,
+  newExpenseH,
+  splitPageH,
+  removeExpenseH,
+  saveExpenseH,
+  editExpensePageH,
 ) where
 
 import Control.Monad.IO.Class (liftIO)
 import Data.Aeson (
-    eitherDecodeStrict,
-    encode,
+  eitherDecodeStrict,
+  encode,
  )
 import Data.ByteString qualified as BS
 import Data.ByteString.Lazy qualified as LBS
+import Data.Text.Encoding (encodeUtf8)
 import Data.Time qualified as Time
 import Data.UUID (UUID)
 import Data.UUID.V4 qualified as UUID
-import Inter.Language (Language)
+import Inter.Language (Language, mkHref)
 import Inter.Lexicon (l)
 import Inter.Lexicon qualified as Lexicon
 import Servant (
-    Handler,
-    NoContent (..),
-    err500,
-    throwError,
+  Handler,
+  NoContent (..),
+  err500,
+  throwError,
  )
 import Server.Env (Env (envTransactionsFile))
 import Server.Split.Html (
-    EditExpensePage (..),
-    Feedback (Success),
-    FeedbackMessage (FeedbackMessage),
-    SplitPage (..),
-    Transactions (..),
+  EditExpensePage (..),
+  Feedback (Success),
+  FeedbackMessage (FeedbackMessage),
+  SplitPage (..),
+  Transactions (..),
  )
 import Server.Utils.Handler (err404', hxRedirect)
 import Split (
-    Expense (expenseDate, expenseId),
-    ExpenseForm,
-    Transaction (..),
-    findExpense,
-    settlements,
-    singleDebtor,
-    toExpense,
+  Expense (expenseDate, expenseId),
+  ExpenseForm,
+  Transaction (..),
+  findExpense,
+  settlements,
+  singleDebtor,
+  toExpense,
  )
 
 settleUpH :: Env -> Language -> Handler Transactions
 settleUpH env lang = do
-    utc <- liftIO Time.getCurrentTime
-    tz <- liftIO Time.getCurrentTimeZone
-    writeTransactionsHandlerHelper
-        env
-        lang
-        (\ts -> map SettlementTransaction (settlements tz utc ts) <> ts)
+  utc <- liftIO Time.getCurrentTime
+  tz <- liftIO Time.getCurrentTimeZone
+  writeTransactionsHandlerHelper
+    env
+    lang
+    (\ts -> map SettlementTransaction (settlements tz utc ts) <> ts)
 
 newExpenseH :: Env -> Language -> ExpenseForm -> Handler Transactions
 newExpenseH env lang form = do
-    utc <- liftIO Time.getCurrentTime
-    tz <- liftIO Time.getCurrentTimeZone
-    uuid <- liftIO UUID.nextRandom
-    writeTransactionsHandlerHelper
-        env
-        lang
-        ( ExpenseTransaction
-            (toExpense form uuid (Time.utcToLocalTime tz utc))
-            :
-        )
+  utc <- liftIO Time.getCurrentTime
+  tz <- liftIO Time.getCurrentTimeZone
+  uuid <- liftIO UUID.nextRandom
+  writeTransactionsHandlerHelper
+    env
+    lang
+    ( ExpenseTransaction
+        (toExpense form uuid (Time.utcToLocalTime tz utc))
+        :
+    )
 
 writeTransactionsHandlerHelper ::
-    Env ->
-    Language ->
-    ([Transaction] -> [Transaction]) ->
-    Handler Transactions
+  Env ->
+  Language ->
+  ([Transaction] -> [Transaction]) ->
+  Handler Transactions
 writeTransactionsHandlerHelper env lang genNewTs = liftIO $ do
-    res <- BS.readFile (envTransactionsFile env)
-    case eitherDecodeStrict res of
-        Right ts -> do
-            let newTs = genNewTs ts
-            LBS.writeFile (envTransactionsFile env) (encode newTs)
-            return (Transactions lang newTs)
-        Left err -> print err >> return (Transactions lang [])
+  res <- BS.readFile (envTransactionsFile env)
+  case eitherDecodeStrict res of
+    Right ts -> do
+      let newTs = genNewTs ts
+      LBS.writeFile (envTransactionsFile env) (encode newTs)
+      return (Transactions lang newTs)
+    Left err -> print err >> return (Transactions lang [])
 
 splitPageH :: Env -> Language -> Handler SplitPage
 splitPageH env lang = liftIO $ do
-    res <- BS.readFile (envTransactionsFile env)
-    case eitherDecodeStrict res of
-        Right ts -> return (SplitPage lang ts)
-        Left err -> print err >> return (SplitPage lang [])
+  res <- BS.readFile (envTransactionsFile env)
+  case eitherDecodeStrict res of
+    Right ts -> return (SplitPage lang ts)
+    Left err -> print err >> return (SplitPage lang [])
 
-removeExpenseH :: Env -> UUID -> Handler NoContent
-removeExpenseH env uuid = do
-    res <- liftIO $ BS.readFile (envTransactionsFile env)
-    case eitherDecodeStrict res of
-        Right ts -> do
-            liftIO $
-                LBS.writeFile
-                    (envTransactionsFile env)
-                    (encode (deleteExpense ts))
-            hxRedirect "/split"
-        Left err -> liftIO (print err) >> throwError err500
-  where
-    deleteExpense :: [Transaction] -> [Transaction]
-    deleteExpense [] = []
-    deleteExpense (x : xs)
-        | ExpenseTransaction e <- x, uuid == expenseId e = xs
-        | otherwise = x : deleteExpense xs
+removeExpenseH :: Env -> Language -> UUID -> Handler NoContent
+removeExpenseH env lang uuid = do
+  res <- liftIO $ BS.readFile (envTransactionsFile env)
+  case eitherDecodeStrict res of
+    Right ts -> do
+      liftIO
+        $ LBS.writeFile
+          (envTransactionsFile env)
+          (encode (deleteExpense ts))
+      hxRedirect $ encodeUtf8 $ mkHref lang "/split"
+    Left err -> liftIO (print err) >> throwError err500
+ where
+  deleteExpense :: [Transaction] -> [Transaction]
+  deleteExpense [] = []
+  deleteExpense (x : xs)
+    | ExpenseTransaction e <- x, uuid == expenseId e = xs
+    | otherwise = x : deleteExpense xs
 
 saveExpenseH :: Env -> Language -> UUID -> ExpenseForm -> Handler EditExpensePage
 saveExpenseH env lang uuid form = do
-    res <- liftIO $ BS.readFile (envTransactionsFile env)
-    case eitherDecodeStrict res of
-        Right ts -> do
-            let newTs = replaceExpense ts
-            liftIO $ LBS.writeFile (envTransactionsFile env) (encode newTs)
-            let mexp = findExpense uuid newTs
-            case (mexp, mexp >>= singleDebtor) of
-                (Just e, Just debtor) ->
-                    return $
-                        EditExpensePage
-                            lang
-                            e
-                            debtor
-                            ( Just
-                                ( FeedbackMessage
-                                    Success
-                                    (l lang Lexicon.ExpenseSaved)
-                                )
-                            )
-                _ ->
-                    throwError $
-                        err404'
-                            lang
-                            (Just (l lang Lexicon.NoSuchExpense))
-        Left err -> liftIO (print err) >> throwError err500
-  where
-    replaceExpense :: [Transaction] -> [Transaction]
-    replaceExpense ((ExpenseTransaction e) : es)
-        | expenseId e == uuid =
-            ExpenseTransaction
-                (toExpense form (expenseId e) (expenseDate e))
-                : es
-        | otherwise = ExpenseTransaction e : replaceExpense es
-    replaceExpense (e : es) = e : replaceExpense es
-    replaceExpense [] = []
+  res <- liftIO $ BS.readFile (envTransactionsFile env)
+  case eitherDecodeStrict res of
+    Right ts -> do
+      let newTs = replaceExpense ts
+      liftIO $ LBS.writeFile (envTransactionsFile env) (encode newTs)
+      let mexp = findExpense uuid newTs
+      case (mexp, mexp >>= singleDebtor) of
+        (Just e, Just debtor) ->
+          return
+            $ EditExpensePage
+              lang
+              e
+              debtor
+              ( Just
+                  ( FeedbackMessage
+                      Success
+                      (l lang Lexicon.ExpenseSaved)
+                  )
+              )
+        _ ->
+          throwError
+            $ err404'
+              lang
+              (Just (l lang Lexicon.NoSuchExpense))
+    Left err -> liftIO (print err) >> throwError err500
+ where
+  replaceExpense :: [Transaction] -> [Transaction]
+  replaceExpense ((ExpenseTransaction e) : es)
+    | expenseId e == uuid =
+        ExpenseTransaction
+          (toExpense form (expenseId e) (expenseDate e))
+          : es
+    | otherwise = ExpenseTransaction e : replaceExpense es
+  replaceExpense (e : es) = e : replaceExpense es
+  replaceExpense [] = []
 
 editExpensePageH :: Env -> Language -> UUID -> Handler EditExpensePage
 editExpensePageH env lang uuid = do
-    res <- liftIO $ BS.readFile (envTransactionsFile env)
-    case eitherDecodeStrict res of
-        Right ts -> do
-            let expense = findExpense uuid ts
-            case expense of
-                Just e -> case singleDebtor e of
-                    Just share -> return $ EditExpensePage lang e share Nothing
-                    Nothing ->
-                        throwError $
-                            err404'
-                                lang
-                                (Just (l lang Lexicon.MaxTwoPeople))
-                Nothing ->
-                    throwError $
-                        err404' lang (Just (l lang Lexicon.NoSuchExpense))
-        Left err -> liftIO (print err) >> throwError err500
+  res <- liftIO $ BS.readFile (envTransactionsFile env)
+  case eitherDecodeStrict res of
+    Right ts -> do
+      let expense = findExpense uuid ts
+      case expense of
+        Just e -> case singleDebtor e of
+          Just share -> return $ EditExpensePage lang e share Nothing
+          Nothing ->
+            throwError
+              $ err404'
+                lang
+                (Just (l lang Lexicon.MaxTwoPeople))
+        Nothing ->
+          throwError
+            $ err404' lang (Just (l lang Lexicon.NoSuchExpense))
+    Left err -> liftIO (print err) >> throwError err500
